0abf9e2cf3c936f8c5da1830f000c71f7a948670
unknown <Josiah Calderon>
2026-05-03 17:54:10 +0800

    Added class Operations

--- main.py
+++ main.py
@@ -8,3 +8,21 @@
 # 7. Use Python Function and appropriate Exceptions to capture errors during   
 #     runtime.
 
+class MDAS:
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2
+
+    def multiply(self):
+        return self.num_1 * self.num_2
+    
+    def divide(self):
+        return self.num_1 / self.num_2
+    
+    def add(self):
+        return self.num_1 + self.num_2
+    
+    def subtract(self):
+        return self.num_1 - self.num_2
+
+print(MDAS(1,2).multiply())
